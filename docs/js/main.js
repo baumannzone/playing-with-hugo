d1510a3312e8ddec2b26ab5c19d9edd6a618fef0
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-10-23 13:02:25 +0200

    Build

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,128 +1,36 @@
-window.onload = function() {
-
-    var $menuIcon = document.getElementsByClassName('menu-icon')[0],
-        $offCanva = document.getElementsByClassName('off-canvas')[0],
-        $siteWrap = document.getElementsByClassName('site-wrapper')[0];
-
-
-    if($menuIcon) {
-        $menuIcon.addEventListener('click', function() {
-            toggleClass($menuIcon, 'close');
-            toggleClass($offCanva, 'toggled');
-            toggleClass($siteWrap, 'open');
-        }, false);
-
-        $menuIcon.addEventListener('mouseenter', function() {
-            addClass($menuIcon, 'hover');
-        });
-
-        $menuIcon.addEventListener('mouseleave', function() {
-            removeClass($menuIcon, 'hover');
-        });
-    }   
-    
-    function addClass(element, className) {
-        element.className += " " + className;
-    }
-
-    function removeClass(element, className) {
-        // Capture any surrounding space characters to prevent repeated
-        // additions and removals from leaving lots of spaces.
-        var classNameRegEx = new RegExp("\\s*" + className + "\\s*");
-        element.className = element.className.replace(classNameRegEx, " ");
-    }
-
-    function toggleClass(element, className) {
-        if (!element || !className) {
-            return;
-        }
-
-        if (element.className.indexOf(className) === -1) {
-            addClass(element, className);
-        } else {
-            removeClass(element, className);
-        }
-    }
-
-
-    // With options
-    let b = baffle('.baffle', {
-        characters: '<$ДГФѾЛѮС▒░/',
-        speed: 75
-    });
-
-    b.reveal(1500);
-      
-    // scroll reveal https://github.com/jlmakes/scrollreveal
-    window.sr = ScrollReveal();
-    sr.reveal('.posts li', {
-        container: '.site-wrapper', 
-        reset: true,
-        mobile: true,
-        easing: 'ease-in-out',
-        scale: 1, 
-        duration: 900,
-        delay: 0,
-        distance: '0px'
-    });
-
-    //smooth-scrollbar
-    var setScrollbar = function(){
-        scrollbar = Scrollbar.init(document.getElementById('full-wrapper'), {
-            speed: 1.0,
-            damping: 0.1,
-            overscrollDamping: 0.2,
-            thumbMinSize: 20,
-            renderByPixels: true,
-            alwaysShowTracks: false,
-            continuousScrolling: 'auto',
-            overscrollEffect: 'bounce',
-            overscrollEffectColor: '#000'
-        });
-    };
-
-    if($(window).width() >= 1025){
-        if(document.getElementById('full-wrapper')){
-            setScrollbar();     
-        }
-    }
-    else{
-        $('.full-wrapper').css({'overflow-y':'scroll', '-webkit-overflow-scrolling':'touch', 'overflow-x':'hidden'});
-    }
-
-    window.onresize = function(event) {
-        if($(window).width() < 1025 && Scrollbar.has(document.getElementById('full-wrapper'))){
-            scrollbar.destroy();
-            $('.full-wrapper').css({'overflow-y':'scroll', '-webkit-overflow-scrolling':'touch', 'overflow-x':'hidden'});
-        }
-        if($(window).width() >= 1025 && !(Scrollbar.has(document.getElementById('full-wrapper')))){
-            if(document.getElementById('full-wrapper')){
-                setScrollbar();     
-            }
-        }
-    };
-
-    // Open Twitter/share in a Pop-Up
-    var $popup = document.getElementsByClassName('popup')[0];
-    if (!$popup) {
-        return;
-    }
-    $popup.addEventListener('click', function(e) {
-        e.preventDefault()
-        var width  = 575,
-            height = 400,
-            left   = (document.documentElement.clientWidth  - width)  / 2,
-            top    = (document.documentElement.clientHeight - height) / 2,
-            url    = this.href,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-
-        window.open(url, 'twitter', opts);
-
-        return false;
-    });
-
-}
+// To make images retina, add a class "2x" to the img element
+// and add a <image-name>@2x.png image. Assumes jquery is loaded.
+ 
+function isRetina() {
+	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+					  (min--moz-device-pixel-ratio: 1.5),\
+					  (-o-min-device-pixel-ratio: 3/2),\
+					  (min-resolution: 1.5dppx)";
+ 
+	if (window.devicePixelRatio > 1)
+		return true;
+ 
+	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
+		return true;
+ 
+	return false;
+};
+ 
+ 
+function retina() {
+	
+	if (!isRetina())
+		return;
+	
+	$("img.2x").map(function(i, image) {
+		
+		var path = $(image).attr("src");
+		
+		path = path.replace(".png", "@2x.png");
+		path = path.replace(".jpg", "@2x.jpg");
+		
+		$(image).attr("src", path);
+	});
+};
+ 
+$(document).ready(retina);
